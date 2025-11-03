35bb089ff344daef621506ac773f5fac2dde391e
Mick-Mick01 <devkha8721@gmail.com>
2025-11-03 17:37:21 +0530

    Implementing mongoDB to flask

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -1,6 +1,19 @@
 from flask import *
 from . import dash_bp
+import pymongo
 
-@dash_bp.route('/')
+client = pymongo.MongoClient("mongodb://localhost:27017")
+db = client.Useless
+collection = db.Working
+
+@dash_bp.route('/', methods=['POST', 'GET'])
 def Index():
-    return "Hello World!"
+    if request.method == 'POST':
+        name = request.form['name']
+        data = {
+            "username":name
+        }
+        collection.insert_one(data)
+        print(name)
+        return redirect(url_for('dashboard.Index'))
+    return render_template("index.html")
